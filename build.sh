1809fc81abc1e2c24954ff005af25b11277684ea
kootsoop <peter@kootsoop.com>
2023-03-06 19:12:58 -0500

    Made builf script add git hash to each HTML file. Ugly, but it works.

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
 touch chapter-1.md
 jupyter-book build . --builder linkcheck
 jupyter-book build -W -n .
+
+git_hash=`cat includes/git_hash.txt`
+for f in ./_build/html/*.html; 
+do 
+	echo "Adding git hash to $f"
+	f_base=`basename $f`
+	sed -e "s/<\!--.*-->//g" $f > /tmp/$f_base.updated
+	echo "<!-- $git_hash -->" >> /tmp/$f_base.updated
+	cp /tmp/$f_base.updated $f
+done
+
