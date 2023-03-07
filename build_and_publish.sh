dc8e5fff0683d74c7df85618eb604dd0f098ebac
kootsoop <peter@kootsoop.com>
2023-03-06 20:28:34 -0500

    Adding published tag to commit that is published.

--- build_and_publish.sh
+++ build_and_publish.sh
@@ -1,6 +1,8 @@
 jupyter-book build -W -n .
 ghp-import -n -p -f _build/html
 git_hash=`cat includes/git_hash.txt`
+git tag -d published
+git push origin :refs/tags/published
 git tag published $git_hash
 git push origin published
 
