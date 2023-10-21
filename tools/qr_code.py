a8a5c1517c68aa51875e035e3e3245a49d002fb2
kootsoop <peter@kootsoop.com>
2023-10-21 16:21:52 -0400

    Changed logo to be QR code for the site.

---
+++ tools/qr_code.py
@@ -0,0 +1,19 @@
+import qrcode
+
+qr = qrcode.QRCode(
+    version = 1,
+    error_correction=qrcode.ERROR_CORRECT_H,
+    box_size=10,
+    border=4,
+)
+qr.add_data('https://circuit-analysis.github.io')
+qr.make(fit=True)
+
+img = qr.make_image(fill_color=(254,204,0), back_color=(32,89,153))
+
+img.save("../logo.png")
+
+
+
+
+
