85e186fd1ff07c3c5f19667387be541bda172036
kootsoop <peter@kootsoop.com>
2023-11-11 19:09:52 -0500

    This does most of what I want. Added some more speelings.

--- tools/three_mesh.py
+++ tools/three_mesh.py
@@ -35,20 +35,41 @@ def get_component():
     return choice
 
 label_by_type = { elm.Resistor: "R", elm.Capacitor: "C", elm.Inductor: "L",
-                 elm.SourceI: "Is", elm.SourceV: "Vs", elm.SourceControlledI: "Ic",
-                 elm.SourceControlledV: "Vc"}
+                 elm.SourceI: "I", elm.SourceV: "V", elm.SourceControlledI: "I",
+                 elm.SourceControlledV: "V"}
 
 
 number_by_type = { elm.Resistor: 0, elm.Capacitor: 0, elm.Inductor: 0,
                  elm.SourceI: 0, elm.SourceV: 0, elm.SourceControlledI: 0,
                  elm.SourceControlledV: 0}
 
-def get_label(component):
+value_by_type = { elm.Resistor: [100,40000,100, "$\Omega$"], elm.Capacitor: [1,1000,1, "$\mu$ F"], elm.Inductor: [1,1000,1,"mH"],
+                 elm.SourceI: [5,500,5,"mA"], elm.SourceV: [4,40,4,"V"], elm.SourceControlledI: [1,10,1,"V$_x$"],
+                 elm.SourceControlledV: [1,20,1,"I$_x$"]}
+
+
+def get_label(component, needs_vx):
     label = label_by_type[type(component)]
     number = number_by_type[type(component)] + 1
     number_by_type[type(component)] = number
     
-    return label+str(number)
+    label = "$" + label + "_" + str(number) + "$"
+    if component == needs_vx:
+        label = ["-", label + "\n$V_x$", "+"] 
+
+    return label
+
+def get_value(component):
+    data = value_by_type[type(component)]
+    return str(random.choice(range(data[0],data[1],data[2]))) + data[3]
+
+def choose_component_that_is_not_a_source(components):
+    choice = random.choice(components)
+    while (isinstance(choice, elm.SourceControlledV) or isinstance(choice, elm.SourceControlledI)
+           or isinstance(choice, elm.SourceV) or isinstance(choice, elm.SourceI)):
+        choice = random.choice(components)
+
+    return choice
 
 # 
 #  *---------[COMPONENT]-----------*
@@ -62,30 +83,37 @@ def get_label(component):
 #  |               |               |
 #  0---------------*---------------*
 #
+CMP1 = get_component()
+CMP2 = get_component()
+CMP3 = get_component()
+CMP4 = get_component()
+CMP5 = get_component()
+CMP6 = get_component()
+
+components = [CMP1, CMP2, CMP3, CMP4, CMP5, CMP6]
+vx_component = ""
+if any(isinstance(x, elm.SourceControlledI) for x in components):
+    vx_component = choose_component_that_is_not_a_source(components)
+    
 
 with schemdraw.Drawing(file='three_mesh.svg') as d:
     d.push()
     d += elm.Line().right()
     d.push()
     d += elm.Line().right()
-    CMP1 = get_component()
-    d += CMP1.up().label(get_label(CMP1))
-    CMP2 = get_component()
-    d += CMP2.left().label(get_label(CMP2))
-    CMP3 = get_component()
-    d += CMP3.left().label(get_label(CMP3))
+    d += CMP1.up().label(get_label(CMP1, vx_component)).label(get_value(CMP1), loc='bottom')
+    d += CMP2.left().label(get_label(CMP2, vx_component)).label(get_value(CMP2), loc='bottom')
+    d += CMP3.left().label(get_label(CMP3, vx_component)).label(get_value(CMP3), loc='bottom')
     d += elm.Line().up()
-    CMP4 = get_component()
-    d += CMP4.right(6).label(get_label(CMP4))
+    d += CMP4.right(6).label(get_label(CMP4, vx_component)).label(get_value(CMP4), loc='bottom')
     d += elm.Line().down()
     d.pop()
-    CMP5 = get_component()
-    d += CMP5.up().label(get_label(CMP5))
+    d += CMP5.up().label(get_label(CMP5, vx_component)).label(get_value(CMP5), loc='bottom')
     d.pop()
-    CMP6 = get_component()
-    d += CMP6.up().label(get_label(CMP6))
-        
+    d += CMP6.up().label(get_label(CMP6, vx_component)).label(get_value(CMP6), loc='bottom')
 
 
+    if any(isinstance(x, elm.SourceControlledV) for x in components):
+        d += elm.CurrentLabelInline(direction='in').at(choose_component_that_is_not_a_source(components)).label('$I_x$')
+    
 
-    #for i in range(1,6):
